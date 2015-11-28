60b5876e8f7c1a84058a25d5351bd0ba349f0bf8
Kiran Koduru <crazycreature11@gmail.com>
2015-11-28 10:46:55 -0500

    Added models and pipelines

---
+++ models.py
@@ -0,0 +1,36 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import scoped_session, sessionmaker
+from sqlalchemy import Column, String, Integer
+
+# db settings
+dbuser = 'user' #DB username
+dbpass = 'password' #DB password
+dbhost = 'localhost' #DB host
+dbname = 'scrapyspiders' #DB database name
+engine = create_engine("mysql://%s:%s@%s/%s?charset=utf8&use_unicode=0"
+                                           %(dbuser, dbpass, dbhost, dbname),
+                                           echo=False,
+                                           pool_recycle=1800)
+db = scoped_session(sessionmaker(autocommit=False,
+                                 autoflush=False,
+                                 bind=engine))
+
+Base = declarative_base()
+
+class AllData(Base):
+    __tablename__ = 'alldata'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String(1000))
+    url = Column(String(1000))
+    desc = Column(String(3000))
+
+    def __init__(self, id=None, title=None, url=None, desc=None):
+        self.id = id
+        self.title = title
+        self.url = url
+        self.desc = desc
+
+    def __repr__(self):
+        return "<AllData: id='%d', title='%s', url='%s', desc='%s'>" % (self.id, self.title, self.url, self.desc)
---
+++ pipelines.py
@@ -0,0 +1,13 @@
+from models import AllData, db
+
+class AddTablePipeline(object):
+
+    def process_item(self, item, spider):
+
+        # create a new SQL Alchemy object and add to the db session
+        record = AllData(title=item['title'][0].decode('unicode_escape'),
+                         url=item['url'][0],
+                         desc=desc)
+        db.add(record)
+        db.commit()
+        return item
